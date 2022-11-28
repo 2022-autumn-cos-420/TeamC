9303f15c519a8ddc5a7b542caedef0f64bb05c9e
Anthony Attaya-Harris <anthony.attayaharris@maine.edu>
2022-11-27 19:03:00 -0500

    Added checking for changing the card on the quiz page

--- memento-with-react/src/pages/QuizPage.test.js
+++ memento-with-react/src/pages/QuizPage.test.js
@@ -11,6 +11,27 @@ function addCard(front, back, hint, deck) {
 }
 
 
+let cardArray1 =  [
+    {id: 0, cardColor: "Red", frontText: "front On2e!", backText: "back Onsdae!", cardHint: "cardHint One!", cardDecks: ["Apples...", "Bananas"]},
+    {id: 1, cardColor: "Red", frontText: "front TW4o!", backText: "back TWo!", cardHint: "cardHint TWO!", cardDecks: ["Apples..."]},
+    {id: 2, cardColor: "Red", frontText: "front TH3REE!", backText: "back Three", cardHint: "cardHint Three!", cardDecks: ["Apples..."]},
+    {id: 3, cardColor: "Red", frontText: "front On6e!", backText: "back One!", cardHint: "cardHint One!", cardDecks: ["Apples..."]},
+    {id: 4, cardColor: "Red", frontText: "front TW5o!", backText: "back TWo!", cardHint: "cardHint TWO!", cardDecks: ["Apples..."]},
+    {id: 5, cardColor: "Red", frontText: "front THR7EE!", backText: "back Three", cardHint: "cardHint Three!", cardDecks: ["Apples..."]},
+    {id: 6, cardColor: "Red", frontText: "front On9e!", backText: "back One!", cardHint: "cardHint One!", cardDecks: ["Apples..."]},
+    {id: 7, cardColor: "Red", frontText: "front TW8o!", backText: "back TWo!", cardHint: "cardHint TWO!", cardDecks: ["Apples..."]},
+    {id: 8, cardColor: "Red", frontText: "front TH0REE!", backText: "back Three", cardHint: "cardHint Three!", cardDecks: ["Apples..."]},
+    {id: 9, cardColor: "Red", frontText: "front On-e!", backText: "back One!", cardHint: "cardHint One!", cardDecks: ["Apples..."]},
+    {id: 10, cardColor: "Red", frontText: "front T=Wo!", backText: "back TWo!", cardHint: "cardHint TWO!", cardDecks: ["Apples..."]},
+    {id: 11, cardColor: "Red", frontText: "front ThHREE!", backText: "back Three", cardHint: "cardHint Three!", cardDecks: ["Apples..."]},
+    {id: 12, cardColor: "Red", frontText: "front Oane!", backText: "back One!", cardHint: "cardHint One!", cardDecks: ["Apples..."]},
+    {id: 13, cardColor: "Red", frontText: "front TbWo!", backText: "back TWo!", cardHint: "cardHint TWO!", cardDecks: ["Apples..."]},
+    {id: 14, cardColor: "Red", frontText: "front THeREE!", backText: "back Three", cardHint: "cardHint Three!", cardDecks: ["Apples..."]}
+  ]
+  
+
+
+
 it("Flashcard Quiz cannot be edited by the user", () => {
     const {queryByTestId} = render(<QuizPage
                                     frontText={"defaultFront"}
@@ -27,5 +48,16 @@ it("Flashcard Quiz cannot be edited by the user", () => {
 
     userEvent.type(backTextInput, "This is a changed back");
     expect(backTextInput.value).toBe("DefaultBack");
-})
+});
 
+it ("QuizPage can succesfully go to the next card", () => {
+    const {queryByTestId} = render(<QuizPage cardArray={cardArray1}>
+                                </QuizPage>)
+        const frontText = screen.queryByTestId("FlashCardFrontText");
+        const nextButton = screen.queryByTestId("CorrectButton");
+        let firstCardFront = frontText.value;
+
+        userEvent.click(nextButton);
+        let secondCardFront = screen.queryByTestId("FlashCardFrontText");
+        expect(firstCardFront !== secondCardFront).toBe(true);
+});

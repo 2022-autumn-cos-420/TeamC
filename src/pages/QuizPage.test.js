9d6f1f5c8ea6755803a22f451fddcbdd1205195a
JoshuaAndle <68359516+JoshuaAndle@users.noreply.github.com>
2022-12-09 16:44:55 -0500

    Update QuizPage.test.js
    Trying to remove {} around filter to match the prop value that is printed out during the rendered version of QuizPage's constructor

--- src/pages/QuizPage.test.js
+++ src/pages/QuizPage.test.js
@@ -132,26 +132,26 @@ describe("QuizPage State Tests", () => {
     test("Passing an empty deck to the quiz page works correctly.", () => {
         const filter = ["All", ""];
         console.log("Creating QuizPage with Filter: ", filter);
-        const quiz = new QuizPage({cardArray: [], updateAccuracy: {updateAccuracy}, filters: {filter} }); 
+        const quiz = new QuizPage({cardArray: [], updateAccuracy: {updateAccuracy}, filters: filter }); 
         expect(deckEquality(quiz.state.cardArray,[])).toEqual(true)
     });
     test("Passing a non-empty deck to the quiz page works correctly.", () => {
         const filter = ["All", ""];
         console.log("Creating QuizPage with Filter: ", filter);
-        const quiz = new QuizPage({cardArray: cardArray1, updateAccuracy: {updateAccuracy}, filters: {filter} }); 
+        const quiz = new QuizPage({cardArray: cardArray1, updateAccuracy: {updateAccuracy}, filters: filter }); 
         expect(deckEquality(quiz.state.cardArray,cardArray1)).toEqual(true)
     });
     // Because the initial index is determined by the scheduler based on accuracy, and card#11 has the lowest accuracy
     test("The initial index of the quizpage is 11", () => {
         const filter = ["All", ""];
         console.log("Creating QuizPage with Filter: ", filter);
-        const quiz = new QuizPage({cardArray: cardArray1, updateAccuracy: {updateAccuracy}, filters: {filter} }); 
+        const quiz = new QuizPage({cardArray: cardArray1, updateAccuracy: {updateAccuracy}, filters: filter }); 
         expect(quiz.state.currentIndex).toEqual(11)
     }); 
     test("The initial card text matches the expected first card of cardArray1", () => {
         const filter = ["All", ""];
         console.log("Creating QuizPage with Filter: ", filter);
-        const quiz = new QuizPage({cardArray: cardArray1, updateAccuracy: {updateAccuracy}, filters: {filter} }); 
+        const quiz = new QuizPage({cardArray: cardArray1, updateAccuracy: {updateAccuracy}, filters: filter }); 
         expect(quiz.state.currentFrontText).toEqual(cardArray1[0].frontText);
         expect(quiz.state.currentBackText).toEqual(cardArray1[0].backText);
         expect(quiz.state.currentCardHint).toEqual(cardArray1[0].cardHint);
